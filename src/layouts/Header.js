6b4d3154dedd05692cc55faf4784396f127ca559
Votre Nom <Vous@exemple.com>
2022-08-30 22:55:18 +0200

    routin problem fixed

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -3,16 +3,26 @@ import "../styles/Header.css"
 import img1 from "../images/1.jpg";
 import img2 from "../images/2.jpg";
 import img3 from "../images/3.jpg";
-import { Route, Switch } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 const Header = () => {
     return (
-        <Switch>
-            <Route path="/products" render={() => (
-                <img src={img2} alt="miasto"/>
-            )} />
-        </Switch>
+        <>
+        <Routes>
+            <Route path="/" exact element={<Image img={img1}/>}/>
+            <Route path="products" element={<Image img={img2}/>}/>
+            <Route path="contact" element={<Image img={img3}/>}/>
+            <Route path="admin" element={<Image img={img1}/>}/>
+            <Route path="*" element={<Image img={img1}/>}/>
+        </Routes>
+        </>
     )
 }
-
+const Image = (props) => {
+    return (
+        <>
+        <img src={props.img} alt="miasto"/>
+        </>
+    )
+}
 export default Header;
