af61cc320eac580cf06ae8a6bc60189034a3d3a8
rafiquee3 <rafiquee.code@gmail.com>
2022-08-30 21:31:32 +0200

    routes problem

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,8 +1,17 @@
 import React from "react";
+import "../styles/Header.css"
+import img1 from "../images/1.jpg";
+import img2 from "../images/2.jpg";
+import img3 from "../images/3.jpg";
+import { Route, Switch } from "react-router-dom";
 
 const Header = () => {
     return (
-        <div>Header</div>
+        <Switch>
+            <Route path="/products" render={() => (
+                <img src={img2} alt="miasto"/>
+            )} />
+        </Switch>
     )
 }
 
