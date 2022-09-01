a04b6fa6c7174283ff6892e52c3996b3905b1a12
rafiquee3 <rafiquee.code@gmail.com>
2022-09-01 15:02:16 +0200

    footer

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -1,9 +1,27 @@
 import React from "react";
+import { Route, Routes, useNavigate, useLocation, useSearchParams } from "react-router-dom";
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <div>
+            <h2>Footer</h2>
+            <Routes>
+                <Route path="/" element={<ShowPath />} />
+                <Route path="/:id" element={<ShowPath />} />
+            </Routes>
+        </div>
     )
 }
-
+const ShowPath = () => {
+    let [searchParams, setSearchParams] = useSearchParams();
+    let location = useLocation();
+    let navigate = useNavigate();
+    console.log(location);
+    console.log(searchParams);
+    return (
+        <>
+            <p>Jestes w {location.pathname}</p>
+        </>
+    )
+}
 export default Footer;
